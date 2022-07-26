03a853cf92bf35c0bcf81ac43b64e02b0ccb2c7e
unknown <dokyung226@gmail.com>
2022-07-27 00:59:26 +0900

    FEAT: Create Project and Set init settings

---
+++ src/main/java/com/oww/OhWoonWanBackend/OhWoonWanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.oww.OhWoonWanBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhWoonWanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhWoonWanBackendApplication.class, args);
+	}
+
+}
